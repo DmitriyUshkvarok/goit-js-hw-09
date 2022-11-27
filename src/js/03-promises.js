c491f06662b45e3917bcd19fb7dd48dd68436ab1
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-11-27 23:51:28 +0200

    fix

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -16,15 +16,15 @@ function createPromise(position, delay) {
 const form = document.querySelector('.form');
 
 form.addEventListener('submit', onFormSubmit);
+
 function onFormSubmit(event) {
   event.preventDefault();
   let delay = Number(event.currentTarget.delay.value);
   let step = Number(event.currentTarget.step.value);
   let amount = Number(event.currentTarget.amount.value);
 
-  for (let i = 0; i < amount; i += 1) {
-    createPromise(i, delay);
-    createPromise(2, 1500)
+  for (let position = 1; position <= amount; position += 1) {
+    createPromise(position, delay)
       .then(({ position, delay }) => {
         Notiflix.Notify.success(
           `✅ Fulfilled promise ${position} in ${delay}ms`
@@ -34,8 +34,8 @@ function onFormSubmit(event) {
         Notiflix.Notify.failure(
           `❌ Rejected promise ${position} in ${delay}ms`
         );
-        delay += step;
       });
+    delay += step;
   }
-  // event.currentTarget.reset();
+  event.currentTarget.reset();
 }
