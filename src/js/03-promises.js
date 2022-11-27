92b9a385c30bd3a30c40f5bca416246a732438f5
DmitriyUshkvarok <dmitriy_ushkvarok@ukr.net>
2022-11-27 17:05:20 +0200

    update

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  let delay = Number(event.currentTarget.delay.value);
+  let step = Number(event.currentTarget.step.value);
+  let amount = Number(event.currentTarget.amount.value);
+
+  for (let i = 0; i < amount; i += 1) {
+    createPromise(i, delay);
+    createPromise(2, 1500)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+        delay += step;
+      });
   }
+  // event.currentTarget.reset();
 }
